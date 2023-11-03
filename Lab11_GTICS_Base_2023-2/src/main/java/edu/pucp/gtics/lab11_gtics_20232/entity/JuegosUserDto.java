5d827d37aeba8ba6c6e86d2d1f8c362802815a5d
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-11-03 17:41:23 -0500

    a

---
+++ Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/entity/JuegosUserDto.java
@@ -0,0 +1,12 @@
+package edu.pucp.gtics.lab11_gtics_20232.entity;
+
+public interface JuegosUserDto {
+
+    Integer getIdjuego();
+    String getImageURL();
+    String getNombre();
+    String getGenero();
+    String getDescripcion();
+
+
+}
