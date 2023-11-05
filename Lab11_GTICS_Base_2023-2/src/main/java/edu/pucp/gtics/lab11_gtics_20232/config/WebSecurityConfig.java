268cb6757cf842ca5d71c6a3cc3c4d0288e36021
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-11-04 22:12:09 -0500

    holiwis

--- Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/config/WebSecurityConfig.java
+++ Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/config/WebSecurityConfig.java
@@ -1,6 +1,26 @@
 package edu.pucp.gtics.lab11_gtics_20232.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-public class WebSecurityConfig  {
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
+        http.httpBasic();
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/product").authenticated()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
 
 }
