a75b9596e2a7cc494ae7b7df246d4609f3806eaa
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-11-04 21:25:31 -0500

    holiwis

--- Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/Daos/EditorasDAO.java
+++ Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/Daos/EditorasDAO.java
@@ -3,18 +3,19 @@ package edu.pucp.gtics.lab11_gtics_20232.Daos;
 import edu.pucp.gtics.lab11_gtics_20232.entity.Editoras;
 import edu.pucp.gtics.lab11_gtics_20232.entity.Generos;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class EditorasDAO {
 
     public List<Editoras> listaEditoras(){
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Editoras[]> response = restTemplate.getForEntity(
-                "http://localhost:8080/editoras", Editoras[].class);
-
+                "http://localhost:8080/editoras/list", Editoras[].class);
         return Arrays.asList(response.getBody());
     }
 
